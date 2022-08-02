a01eff07fce28684ec35ec7884472381f969408e
alicialowe <hohpascal@outlook.com>
2022-08-02 11:44:46 +0100

    added in index.js connection info, connection to db doesn't seem to be working

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-import express, { application } from "express";
+import express from "express";
 import 'dotenv/config';
-import pg from "pg";
+//import pg from "pg";
 
-
-
+const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
+// test route
+app.get("/", function (req, res) {
+    res.json({
+      success: true,
+      message: "Test route up and running!",
+    });
+  });
 
 
+//come from routes.js  
+//app.use("/", router);
 
 
-
-
-
-
-
-application.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server listening at port: ${port}`)
 });
+
+export default app;
---
+++ db/index.js
@@ -0,0 +1,21 @@
+import pg from "pg";
+import 'dotenv/config';
+
+const pool = new pg.Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    
+    
+    //connectionString: process.env.PGCONNECTIONSTRING,
+    ssl: {rejectUnauthorized: false},
+});
+
+export function query(text, params, callback) {
+    return pool.query(text, params, callback);
+}
+
+const result = await pool.query(`SELECT NOW()`);
+console.log(result.rows);
