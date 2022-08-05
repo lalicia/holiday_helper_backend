58cd3807fb841240fe87404a38d65e98e73dbc6e
mateuszsoszynski <soszynski94@googlemail.com>
2022-08-05 09:31:26 +0100

    GET all items ASC order request added

--- models/models.js
+++ models/models.js
@@ -4,3 +4,8 @@ export async function getAllItems(){
     const res = await pool.query(`SELECT * FROM emergency_contacts;`)
     return res.rows;
 }
+
+export async function getAllItemsASC(){
+    const res = await pool.query(`SELECT * FROM emergency_contacts ORDER BY country ASC;`)
+    return res.rows
+}
--- routes/route.js
+++ routes/route.js
@@ -14,4 +14,14 @@ emergencyContactsRouter.get("/", async function (req, res) {
     res.status(200).json(responseObject);
 });
 
+emergencyContactsRouter.get("/asc", async function (req, res) {
+    console.log("GET request for all items ASC order")
+    const responseObject = {
+        success: true,
+        message: "Returned all items ASC order",
+        payload: await getAllItemsASC()
+    };
+    res.status(200).json(responseObject);
+});
+
 export default emergencyContactsRouter;
